b7bf3b7de73c0e17bcdd31d259fab1840e6271c6
Cj <ctiwari@lakeheadu.ca>
2024-10-21 10:46:18 -0400

    Added simplicity for number of cars

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.linalg import block_diag
 
 # Kalman filter function
 def kalman_filter(X, P, A, B, U, H, Q, R, Z):
+
+    epsilon =  1e-5
+    lambda_val = 0.005
+
     # Prediction step
     X_pred = np.dot(A, X) + np.dot(B, U)
     P_pred = np.dot(A, np.dot(P, A.T)) + Q
@@ -11,10 +16,49 @@ def kalman_filter(X, P, A, B, U, H, Q, R, Z):
     S = np.dot(H, (np.dot(P_pred, H.T) + R))
     K = np.dot(P_pred, np.dot(H.T, np.linalg.inv(S)))
 
-    X_new = X_pred + np.dot(K, (Z - np.dot(H, X_pred)))
+    residual = Z - np.dot(H, X_pred)
+
+    z_bar = np.vstack((residual, X_pred))
+
+    H_bar = np.vstack((H, H))
+
+    # PWLS Iterative Process
+    w_est = np.vstack((np.zeros((len(residual), 1)), np.ones((len(residual), 1))))  # Start with all weights as 1
+    eps = 1
+    X_plus = X_pred.copy()  # Initialize with predicted state
+
+    Sk = block_diag(R, P_pred)
+
+    X_new = X_plus
+
+    
+    while eps > epsilon:
+        Z_adj = np.dot(np.diag(w_est.flatten()), z_bar)
+        H_adj = np.dot(np.diag(w_est.flatten()), H_bar)
+
+        X_old = X_new
+
+        M = H_adj.T @ np.linalg.inv(Sk) @ H_adj
+        X_new = np.linalg.inv(M) @ (H_adj.T @ np.linalg.inv(Sk) @ Z_adj)
+
+        residual_new = z_bar - H_bar @ X_new
+        
+        X_plus = X_new.copy()
+
+        w_est = np.maximum(np.minimum(np.sqrt(0.5 * lambda_val) / np.abs(residual_new), 1), 0)
+        eps = np.linalg.norm(X_old - X_new, np.inf)
+
+
+
+    Z_adj = np.dot(np.diag(w_est.flatten()), z_bar)
+    H_adj = np.dot(np.diag(w_est.flatten()), H_bar)
+    X_old = X_new
+    M = H_adj.T @ np.linalg.inv(Sk) @ H_adj
+    X_new = np.linalg.inv(M) @ (H_adj.T @ np.linalg.inv(Sk) @ Z_adj)
     P_new = np.dot((np.eye(len(K)) - np.dot(K, H)), P_pred)
 
     return X_new, P_new
+
 
 # Function to dynamically create matrices based on N cars
 def get_matrices(N, K, B, H, I):
@@ -95,16 +139,21 @@ def simulate_platoon(N, I, li, xi, vi, ai, desired_distance, C, K, B, H, tau, si
 
     # Initialize arrays to store positions, velocities, and accelerations of all cars over time
     cars = np.zeros((num_of_iter, N, 3))
+    cars_original = np.zeros((num_of_iter, N, 3))
 
     # Initial conditions for car 0 (leader)
     X = np.concatenate((del_xi, del_vi, del_ai), axis=1).transpose()
     X_current = X.copy()
+
+    # attack_iter = [1000, 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1500, 1501, 1502, 1503, 1504, 1505]
 
     # Kalman Filter Variables
     H_kalman = np.eye(3 * (N-1))
     Q = np.eye(3 * (N-1)) * 1e-5  # Process noise covariance
     R = np.eye(3 * (N-1)) * 1e-5  # Measurement noise covariance
     P = np.eye(3 * (N-1)) * 1e-5  # Initial state covariance
+
+    noise_deviation = 0.5
 
     car_0_position = xi[0]
     car_0_velocity = vi[0]
@@ -120,22 +169,41 @@ def simulate_platoon(N, I, li, xi, vi, ai, desired_distance, C, K, B, H, tau, si
         U = get_U(N, K, I)
 
         X_cap = np.matmul(A_matrix, X_current) + np.matmul(B_matrix, U)
+  
+        # if iter in attack_iter:
+        #     noise = np.random.normal(0, 200, X_cap.shape)
+        # else:
+        #     noise = np.random.normal(0, noise_deviation, X_cap.shape)
 
 
-        # Currently adding manual noise to the system
-        noise = np.random.normal(0, 0.5, X_cap.shape)
         # Kalman filter measurement: here we assume the measurement Z to be the current state
         Z = X_cap.copy() 
-        Z = Z + noise
+        Z = Z + 10
 
         # Apply Kalman filter
         X_new, P = kalman_filter(X_current, P, A_matrix, B_matrix, U, H_kalman, Q, R, Z)
+
+
+        def compare_with_threshold(matrix1, matrix2, threshold):
+            # Compute the absolute difference between the matrices
+            difference = np.abs(matrix1 - matrix2)
+            
+            # Check if the difference exceeds the threshold
+            result = np.any(difference > threshold)
+            return result
+
+        if compare_with_threshold(Z, X_new, 50):
+            print(f"Attack detected at iteration {iter}.")
+
         
         # Update X_current with Kalman filter output
         X_cap = X_new.copy()
         X_current = X_current + time_step * X_cap
 
+        X_noisy = X_current + time_step * Z
+
         plot_data = X_current.copy().reshape(-1, N-1)
+        plot_noisy = X_noisy.copy().reshape(-1, N-1)
 
         car_0_position += car_0_velocity * time_step + 0.5 * car_0_acceleration * time_step**2
         car_0_velocity += car_0_acceleration * time_step
@@ -152,6 +220,18 @@ def simulate_platoon(N, I, li, xi, vi, ai, desired_distance, C, K, B, H, tau, si
                 cars[iter][car][1] = car_0_velocity + plot_data[1][N-car-1]  # Velocity
                 cars[iter][car][2] = car_0_acceleration + plot_data[2][N-car-1]  # Acceleration
 
+        # Update the positions, velocities, and accelerations in the storage array
+        for car in range(N):
+            if car == 0:
+                cars_original[iter][car][0] = car_0_position
+                cars_original[iter][car][1] = car_0_velocity
+                cars_original[iter][car][2] = car_0_acceleration
+            else:
+                cars_original[iter][car][0] = car_0_position + plot_noisy[0][N-car-1]  # Position
+                cars_original[iter][car][1] = car_0_velocity + plot_noisy[1][N-car-1]  # Velocity
+                cars_original[iter][car][2] = car_0_acceleration + plot_noisy[2][N-car-1]  # Acceleration
+
+    # Plot for correct
 
     plt.figure(figsize=(10, 6))
     for car in range(N):
@@ -186,10 +266,46 @@ def simulate_platoon(N, I, li, xi, vi, ai, desired_distance, C, K, B, H, tau, si
     plt.grid(True)
     plt.savefig("platoon_accelerations.png")
 
+
+    # Plot for noisy
+
+    plt.figure(figsize=(10, 6))
+    for car in range(N):
+        plt.plot(time_array, cars_original[:, car, 0], label=f"Car {car} (Follower)", linestyle='-')
+    
+    plt.xlabel("Time (s)")
+    plt.ylabel("Position (m)")
+    plt.title("Position of Cars in Platoon over Time (Noisy)")
+    plt.legend()
+    plt.grid(True)
+    plt.savefig("platoon_positions_noisy.png")
+
+    plt.figure(figsize=(10, 6))
+    for car in range(N):
+        plt.plot(time_array, cars_original[:, car, 1], label=f"Car {car} (Follower)", linestyle='-')
+    
+    plt.xlabel("Time (s)")
+    plt.ylabel("Velocity (m/s)")
+    plt.title("Velocity of Cars in Platoon over Time (Noisy)")
+    plt.legend()
+    plt.grid(True)
+    plt.savefig("platoon_velocities_noisy.png")
+
+    plt.figure(figsize=(10, 6))
+    for car in range(N):
+        plt.plot(time_array, cars_original[:, car, 2], label=f"Car {car} (Follower)", linestyle='-')
+    
+    plt.xlabel("Time (s)")
+    plt.ylabel("Acceleration (m/s2)")
+    plt.title("Acceleration of Cars in Platoon over Time (Noisy)")
+    plt.legend()
+    plt.grid(True)
+    plt.savefig("platoon_accelerations_noisy.png")
+
     return cars
 
 # Example usage
-N = 7  # Number of cars, you can increase this
+N = 5  # Number of cars, you can increase this
 
 # Initial conditions (generalize for N cars)
 li = np.array([4, 4.4, 3.8, 5.2, 4.4, 3.8, 4])
@@ -204,9 +320,9 @@ C = []
 for i in I:
     C.append(len(i))
 
-K = [3] * N
-B = [5] * N
-H = [1] * N
-tau = [0.5] * N
+K = [3] * (N)
+B = [5] * (N)
+H = [1] * (N)
+tau = [0.5] * (N)
 
-simulate_platoon(N, I, li, xi, vi, ai, desired_distance, C, K, B, H, tau)
+simulate_platoon(N, I, li[:N], xi[:N], vi[:N], ai[:N], desired_distance[:N], C, K, B, H, tau)
